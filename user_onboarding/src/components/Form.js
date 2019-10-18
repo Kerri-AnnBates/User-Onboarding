7f449ac5ef7fb9b38838b0d899ec85b648de29ba
Kerri-Ann <kaybee1jam@yahoo.com>
2019-10-17 20:06:41 -0400

    completed post request

--- user_onboarding/src/components/Form.js
+++ user_onboarding/src/components/Form.js
@@ -1,28 +1,48 @@
 import React, { useState } from 'react';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from "yup";
+import axios from 'axios';
 
-// Name
-// Email
-// Password
-// Terms of Service(checkbox)
-// A Submit button to send our form data to the server.
-
-function UserForm({ values }) {
-    // const [user, setUser] = useState('');
+function UserForm({ values, errors, touched }) {
+    const [user, setUser] = useState({});
     return (
         <Form>
             <Field type="text" name="name" placeholder="Enter Name" />
+            {touched.name && errors.name && <p className="error">{errors.name}</p>}
             <Field type="email" name="email" placeholder="Enter Email" />
+            {touched.email && errors.email && <p className="error">{errors.email}</p>}
             <Field type="password" name="password" placeholder="Enter Password" />
-            <Field  type="checkbox" name="terms" /> <label htmlFor="terms">Terms of Service</label>
+            {touched.name && errors.name && <p className="error">{errors.password}</p>}
+            <label htmlFor="terms"><Field  type="checkbox" name="terms" checked={values.terms} id="terms" /> Terms of Service</label>
             <button type="submit">Submit</button>
         </Form>
     )
 }
 
 const FormikUserForm = withFormik({
-
+    mapPropsToValues: ({ name, email, password, terms }) => {
+        return {
+            name: name || '',
+            email: email || '',
+            password: password || '',
+            terms: terms || false
+        }
+    },
+    validationSchema: Yup.object().shape({
+        name: Yup.string().required('Enter your name'),
+        email: Yup.string().email().required('Enter your email'),
+        password: Yup.string().min(4, 'Password must be longer than 4 characters').required('Enter your password')
+    }),
+    handleSubmit(values, { resetForm }) {
+        axios.post('https://reqres.in/api/users', values)
+        .then(response => {
+            console.log(response.values);
+            resetForm();
+        }).catch(error => {
+            console.log(error);
+        })
+        console.log(values);
+    }
 })(UserForm);
 
 export default FormikUserForm;
